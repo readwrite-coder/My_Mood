23d53c973561575ec49c9434b564bddc0113b0ec
atrinh96 <trinh.96.andrew@gmail.com>
2020-02-01 20:31:40 -0800

    GIT-initialization

---
+++ app/src/main/java/usc/health/mymood/MainActivity.java
@@ -0,0 +1,15 @@
+package usc.health.mymood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* comments to see if VCS is working */
+    }
+}
